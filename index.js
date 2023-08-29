d97748cd9acb7ccd46e2cd4dcc31e843cd025042
JongHun, Lim <leemhoon000@gmail.com>
2023-08-29 17:33:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function handler(event) {
     try{
         var request = event.request;
-        var origin = request.headers.origin.value; // Note: Optional chaining (?.) syntax is not supported in this version of Node.js used by CF Functions.
+        // Note: Optional chaining (?.) syntax is not supported in this version of Node.js used by CF Functions.
+        var origin = request.headers.origin.value; // Note: If 'value' property is absent, this will throw an error caught by the 'catch' block.
     
-        
-        if (origin === 'http://localhost:5173') { // your Origin
+        // Allowing specific Origin
+        if (origin === 'http://localhost:5173') { 
             return request; // Allow
         } else {
             return {
@@ -14,6 +15,7 @@ function handler(event) {
             };
         }
     } catch (error) {
+        // Catching errors when origin.value is not present (handling absence of header)
         return {
             statusCode: 403,
             statusDescription: 'Authorization',
